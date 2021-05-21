c1b364fe0c604c7063bb34d9f9e13fee9e0fa3aa
Evgenii Sopov <mrseakg@gmail.com>
2021-05-21 10:32:47 +0700

    Updated version to 0.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='roadmapgen2d',
-    version='0.0.1',
+    version='0.0.2',
     install_requires=[],
     keywords=['roadmap', 'generator', '2d'],
     author='Evgenii Sopov',
